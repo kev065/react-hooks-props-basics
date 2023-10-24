7c1b2f8a529358eaae7a54fbc2baf6e81602d93a
KELVIN MURITHI <kelvinmurithi@live.com>
2023-10-24 20:10:01 +0300

    updated

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -2,7 +2,18 @@ import React from "react";
 
 function BlogContent(props) {
   console.log(props);
-  return <div id="blog-content">{props.articleText}</div>;
+  if (!props.isPublished) {
+    return null;
+
+  } else {
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }
 }
+
 
 export default BlogContent;
